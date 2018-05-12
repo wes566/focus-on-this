d7da6c3ce5ee3eba451f32f2238fa9b04ec91786
Wes Peter <wes@wespeter.com>
2018-05-12 15:13:20 -0700

    modify some stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,51 @@
 import FocusPage from "components/FocusPage";
+import grey from "material-ui/colors/grey";
+// import blueGrey from "material-ui/colors/blueGrey";
+import indigo from "material-ui/colors/indigo";
+import CssBaseline from "material-ui/CssBaseline/CssBaseline";
 import { createMuiTheme, MuiThemeProvider } from "material-ui/styles";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import { HashRouter as Router } from "react-router-dom";
-// import { injectGlobal, ThemeProvider } from "styled-components";
+import { injectGlobal } from "styled-components";
 import registerServiceWorker from "./registerServiceWorker";
 
 const theme = createMuiTheme({
   palette: {
-    primary: {
-      light: "#757ce8",
-      main: "#3f50b5",
-      dark: "#002884",
-      contrastText: "#fff"
-    },
-    secondary: {
-      light: "#ff7961",
-      main: "#f44336",
-      dark: "#ba000d",
-      contrastText: "#000"
-    }
+    primary: indigo,
+    secondary: grey
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(",")
   }
 });
 
 // tslint:disable-next-line:no-unused-expression
-// injectGlobal`
-// body {
-//   margin: 0;
-//   padding: 0;
-//   font-family: sans-serif;
-//   height: 100%;
-// }
-// html,
-// #root {
-//   height: 100%;
-// }
-// `;
+injectGlobal`
+body {
+  position: fixed;
+}
+`;
 
 const render = component => {
   ReactDOM.render(
     <AppContainer>
       <Router>
         <MuiThemeProvider theme={theme}>
+          <CssBaseline />
           <FocusPage />
         </MuiThemeProvider>
       </Router>

cf29190e7b23fdc407a757dbc17f00ea49dab95b
Wes Peter <wes@wespeter.com>
2019-10-29 23:37:19 -0700

    hover effect on close button

--- public/js/components/fot-drawer.js
+++ public/js/components/fot-drawer.js
@@ -42,6 +42,10 @@ class FotDrawer extends LitElement {
         justify-content: space-between;
         align-items: center;
       }
+
+      .icon-button:active {
+        transform: translateY(4px);
+      }
     `;
   }
 
@@ -56,20 +60,22 @@ class FotDrawer extends LitElement {
         <div class="sxs">
           <h2>Focus On This</h2>
           <!-- TODO - svg should be wrapped in a button for a11y -->
-          <svg
-            @click="${this.handleCloseClick}"
-            class="i-close"
-            viewBox="0 0 32 32"
-            width="24"
-            height="24"
-            fill="none"
-            stroke="currentcolor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-          >
-            <path d="M2 30 L30 2 M30 30 L2 2"></path>
-          </svg>
+          <div class="icon-button">
+            <svg
+              @click="${this.handleCloseClick}"
+              class="i-close"
+              viewBox="0 0 32 32"
+              width="24"
+              height="24"
+              fill="none"
+              stroke="currentcolor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+            >
+              <path d="M2 30 L30 2 M30 30 L2 2"></path>
+            </svg>
+          </div>
         </div>
         <h4>A simple app to help you focus on one thing</h4>
       </div>
